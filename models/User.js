7be1754a7338527e414cceb556108ee2ef1038f3
Stevens Lloyd <stevens91@gmail.com>
2025-03-10 15:53:38 +0100

    feat: user models

---
+++ models/User.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    cartItems: {
+      type: Object,
+      default: {},
+    },
+  },
+  { minimize: false }
+);
+
+const User = mongoose.models.user || mongoose('user', userSchema)
+
+export default User;
